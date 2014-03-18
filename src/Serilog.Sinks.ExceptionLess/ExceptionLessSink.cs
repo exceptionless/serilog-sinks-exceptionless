c1ea83b5b68cccf927bf623fa66c9ab09d5b9917
Phil Scott <phil.scott@cbeyond.net>
2014-03-18 16:33:16 -0400

    Updating to ExceptionLess 1.4 and Serilog 1.2.29

--- src/Serilog.Sinks.ExceptionLess/ExceptionLessSink.cs
+++ src/Serilog.Sinks.ExceptionLess/ExceptionLessSink.cs
@@ -1,32 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Exceptionless;
-using Serilog.Core;
-using Serilog.Events;
-
-namespace Serilog.Sinks.ExceptionLess
+﻿namespace Serilog.Sinks.ExceptionLess
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Core;
+    using Events;
+    using Exceptionless;
+
+    /// <summary>
+    /// ExceptionLess Sink
+    /// </summary>
     public class ExceptionLessSink : ILogEventSink
     {
+        /// <summary>
+        /// Additional operation to execute against the <c>ErrorBuilder</c> object prior to submitting to ExceptionLess
+        /// </summary>
         private readonly Func<ErrorBuilder, ErrorBuilder> _additionalOperation;
+
+        /// <summary>
+        /// If false then the seri log properties will not be submitted to ExceptionLess
+        /// </summary>
         private readonly bool _includeProperties;
 
-        public ExceptionLessSink( Func<ErrorBuilder, ErrorBuilder> additionalOperation = null, bool includeProperties= true)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExceptionLessSink"/> class.
+        /// </summary>
+        /// <param name="additionalOperation">
+        /// Optional operation to run against the Error Builder before submitting to ExceptionLess
+        /// </param>
+        /// <param name="includeProperties">
+        /// If false then the seri log properties will not be submitted to ExceptionLess
+        /// </param>
+        public ExceptionLessSink(Func<ErrorBuilder, ErrorBuilder> additionalOperation = null, bool includeProperties= true)
         {
-            _additionalOperation = additionalOperation;
-            _includeProperties = includeProperties;
+            this._additionalOperation = additionalOperation;
+            this._includeProperties = includeProperties;
         }
 
         public void Emit(LogEvent logEvent)
         {
             if (logEvent == null)
+            {
                 throw new ArgumentNullException("logEvent");
+            }
 
             if (logEvent.Exception == null)
+            {
                 return;
+            }
 
             ErrorBuilder errorBuilder = logEvent.Exception.ToExceptionless();
             if (logEvent.Level == LogEventLevel.Fatal)
@@ -36,16 +59,18 @@ namespace Serilog.Sinks.ExceptionLess
 
             errorBuilder.AddObject(logEvent.RenderMessage(), "Log Message");
 
-            if (_includeProperties && logEvent.Properties != null && logEvent.Properties.Count != 0)
+            if (this._includeProperties && logEvent.Properties != null && logEvent.Properties.Count != 0)
             {
                 foreach (var property in logEvent.Properties)
                 {
                     errorBuilder.AddObject(property.Value, property.Key);
                 }                
             }
-            
-            if (_additionalOperation != null)
-                _additionalOperation(errorBuilder);
+
+            if (this._additionalOperation != null)
+            {
+                this._additionalOperation(errorBuilder);
+            }
 
             errorBuilder.Submit();
         }
--- src/Serilog.Sinks.ExceptionLess/LoggerConfigurationExceptionLessExtensions.cs
+++ src/Serilog.Sinks.ExceptionLess/LoggerConfigurationExceptionLessExtensions.cs
@@ -1,15 +1,37 @@
-using System;
-using Exceptionless;
-using Serilog.Configuration;
-
 namespace Serilog.Sinks.ExceptionLess
 {
+    using System;
+    using Configuration;
+    using Exceptionless;
+    
+    /// <summary>
+    /// The logger configuration exception less extensions.
+    /// </summary>
     public static class LoggerConfigurationExceptionLessExtensions
     {
+        /// <summary>
+        /// Creates a new ExceptionLess sink
+        /// </summary>
+        /// <param name="loggerConfiguration">
+        /// The logger configuration.
+        /// </param>
+        /// <param name="additionalOperation">
+        /// Any additional operation to run against the build exceptions
+        /// </param>
+        /// <param name="includeProperties">
+        /// If false it suppressed sending the Serilog properties to ExceptionLess
+        /// </param>
+        /// <returns>
+        /// The <see cref="LoggerConfiguration"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// </exception>
         public static LoggerConfiguration ExceptionLess(this LoggerSinkConfiguration loggerConfiguration, Func<ErrorBuilder, ErrorBuilder> additionalOperation = null, bool includeProperties = true)
         {
             if (loggerConfiguration == null)
+            {
                 throw new ArgumentNullException("loggerConfiguration");
+            }
 
             return loggerConfiguration.Sink(new ExceptionLessSink(additionalOperation, includeProperties));
         }
