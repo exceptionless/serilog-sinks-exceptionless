4639bbb3b3a568e4c5fd61d82b56b260d922bd46
Blake Niemyjski <bniemyjski@gmail.com>
2016-07-07 12:31:16 -0500

    Fixed semver issue

--- Build.ps1
+++ Build.ps1
@@ -4,11 +4,13 @@ if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
 
 & dotnet restore
 
-$revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
+$branch = @{ $true = $env:APPVEYOR_REPO_BRANCH; $false = $(git symbolic-ref --short -q HEAD) }[$env:APPVEYOR_REPO_BRANCH -ne $NULL];
+$revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:APPVEYOR_BUILD_NUMBER, 10); $false = "local" }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
+$suffix = @{ $true = ""; $false = "$branch-$revision"}[$branch -eq "master" -and $revision -ne "local"]
 
 Push-Location src/Serilog.Sinks.Exceptionless
 
-& dotnet pack -c Release -o ..\..\.\artifacts --version-suffix=$revision
+& dotnet pack -c Release -o ..\..\.\artifacts --version-suffix=$suffix
 if($LASTEXITCODE -ne 0) { exit 1 }    
 
 Pop-Location
